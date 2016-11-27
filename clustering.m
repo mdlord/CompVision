20737b2f4b959460306b6610322a805e54c33521
Mayank Daswani <mayankdaswani123@yahoo.com>
2016-11-27 02:03:20 -0500

    Add files via upload

--- clustering.m
+++ clustering.m
@@ -1,30 +1,67 @@
 %reading all images of the directory
-step = 15;
-bin_size = 8;
+imgDir= 'vision_dataset';
+imds = imageDatastore(imgDir,'IncludeSubfolders', true,'LabelSource','foldernames')
+
+tbl = countEachLabel(imds);
+
+mincount = min(tbl{:,2});
+
+imds = splitEachLabel(imds, mincount, 'randomize');
+
+countEachLabel(imds)
+
+[trainingSet, validationSet] = splitEachLabel(imds, 0.3, 'randomize');
+
+% Find the first instance of an image for each category
+
+%airplanes = find(trainingSet.Labels == 'airplanes', 1);
+%ferry = find(trainingSet.Labels == 'ferry', 1);
+%laptop = find(trainingSet.Labels == 'laptop', 1);
+
+%step = 15;
+%bin_size = 8;
+%f = [];
 features = [];
-pics = dir('*.jpg');
-total_files=length(pics);
+%pics = dir('*.jpg');
+%total_files=length(pics);
 
 K = 100;
 dimension = 2;
 
 %loop for the extractiong interest points
 %variable SIFT_features stores the descriptors
-for i = 1:total_files
-    img = single( imread(pics(i).name));
-    if size(img, 3) > 1
+for i = 1:size(trainingSet.Files)
+    
+    img = imread(trainingSet.Files{i});
+    if (size(img, 3) > 1)
         img =rgb2gray(img);
     end
-    figure(i);
-    imshow(pics(i).name)
-    [f,d] = vl_sift(I_gray);
-    [locations, SIFT_features] = vl_dsift(img, 'fast', 'step', step, 'size', bin_size);
-    features = [features, SIFT_features];
+    
+    img = im2single(img);
+    
+    %lab 
+    [f,d] = vl_sift(img);
+    
+    %[locations, SIFT_features] = vl_dsift(img);
+    %features = [features, SIFT_features];
     
     %selects few of the interest points to display
-    perm = randperm(size(f,2)) ;
-    sel = perm(1:100) ;
-    h1 = vl_plotsiftdescriptor(d(:,sel),f(:,sel)) ;
+    %perm = randperm(size(f,2)) ;
+    %sel = perm(1:100) ;
+    
+    if(i<5)
+        figure(i);
+        subplot(1,2,1)
+        imshow(img)
+        hold on;
+        h1 = vl_plotsiftdescriptor(d,f) ;
+        hold off;
+        
+        subplot(1,2,2)
+        histogram(d(:,i), 50);
+    end
+           
+        features = [features,im2single(d)]; 
 
 end
 
@@ -59,5 +96,3 @@ for i = 1:total_files
 end
 
 
-
--
